c37a07242cc4a79e68d0ad5bc0340b0680f73b29
Andrew Cantino <cantino@users.noreply.github.com>
2020-05-25 14:50:40 -0700

    Ensure correct HISTFILE when developing

--- dev.bash
+++ dev.bash
@@ -3,10 +3,12 @@
 
 this_dir=$(cd `dirname "$0"`; pwd)
 
-rm -r target/debug/deps/mcfly-*
+rm -f target/debug/mcfly
+rm -rf target/debug/deps/mcfly-*
 cargo build
 # For some reason, to get line numbers in backtraces, we have to run the binary directly.
-MCFLY_PATH=$(find target/debug/deps/mcfly-* -maxdepth 1 -type f | grep -v '\.d') \
+HISTFILE=$HOME/.bash_history \
+  MCFLY_PATH=$(find target/debug/deps/mcfly-* -maxdepth 1 -type f | grep -v '\.d') \
   RUST_BACKTRACE=full \
   MCFLY_DEBUG=1 \
   exec /bin/bash --init-file "$this_dir/mcfly.bash" -i
--- dev.zsh
+++ dev.zsh
@@ -9,10 +9,12 @@ touch $this_dir/.zsh_history
 # Needed so that the test instance of zsh sources the local mcfly.zsh file on startup.
 echo "source ./mcfly.zsh" > $this_dir/.zshrc
 
-rm -r target/debug/deps/mcfly-*
+rm -f target/debug/mcfly
+rm -rf target/debug/deps/mcfly-*
 cargo build
 # For some reason, to get line numbers in backtraces, we have to run the binary directly.
-MCFLY_PATH=$(find target/debug/deps/mcfly-* -maxdepth 1 -type f | grep -v '\.d') \
+HISTFILE=$HOME/.zsh_history \
+  MCFLY_PATH=$(find target/debug/deps/mcfly-* -maxdepth 1 -type f | grep -v '\.d') \
   RUST_BACKTRACE=full \
   MCFLY_DEBUG=1 \
   ZDOTDIR="$this_dir" \
