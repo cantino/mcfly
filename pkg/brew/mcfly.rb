818030f493825e1a02cb2604a8a7acaa582a8db0
Andrew Cantino <cantino@users.noreply.github.com>
2021-04-01 11:59:15 -0700

    Update homebrew for 0.5.6

--- pkg/brew/mcfly.rb
+++ pkg/brew/mcfly.rb
@@ -7,16 +7,16 @@
 #   brew untap cantino/mcfly
 
 class Mcfly < Formula
-  version 'v0.5.5'
+  version 'v0.5.6'
   desc "McFly"
   homepage "https://github.com/cantino/mcfly"
 
   if OS.mac?
     url "https://github.com/cantino/mcfly/releases/download/#{version}/mcfly-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "2ba77cd5cf231817322eb2a08256614d6b296a8999d3809ac8053f3b29544bde"
+    sha256 "de0f830ed5432b092c7d10b5c587055493373dff9f6f0ac2b0692693ed0ab58b"
   elsif OS.linux?
     url "https://github.com/cantino/mcfly/releases/download/#{version}/mcfly-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "ae41010e77592ed96a082c449dfa2a31b6a2f96cf1b6380f4478e5395e5210ba"
+    sha256 "aed3f23d4a307bd21c2346dc63f48972b49acf96fb3afa9b9e63734081f21a76"
   end
 
   def install
