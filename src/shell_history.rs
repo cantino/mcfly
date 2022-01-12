56101f8f54218de6c149e343c7583a22aa71bbf6
Andrew Cantino <cantino@users.noreply.github.com>
2022-01-12 10:43:02 -0800

    Bumping to v0.5.12

--- src/shell_history.rs
+++ src/shell_history.rs
@@ -247,8 +247,8 @@ pub fn append_history_entry(command: &HistoryCommand, path: &Path, debug: bool) 
         .open(path)
         .unwrap_or_else(|err| {
             panic!(
-                "McFly error: please make sure HISTFILE/MCFLY_HISTFILE exists ({})",
-                err
+                "McFly error: please make sure HISTFILE/MCFLY_HISTFILE ({:?}) exists ({})",
+                path, err
             )
         });
 
