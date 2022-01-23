69fd1f2c606fa8fb5d8abbcc554664dde66666b1
Andrew Cantino <cantino@users.noreply.github.com>
2022-01-22 20:36:59 -0800

    Try conversion

--- src/fake_typer.rs
+++ src/fake_typer.rs
@@ -1,14 +1,16 @@
 use libc;
+use std::convert::TryInto;
 
 // Should we be using https://docs.rs/libc/0.2.44/libc/fn.ioctl.html instead?
 extern "C" {
     pub fn ioctl(fd: libc::c_int, request: libc::c_ulong, arg: ...) -> libc::c_int;
 }
 
+#[allow(clippy::useless_conversion)]
 pub fn use_tiocsti(string: &str) {
     for byte in string.as_bytes() {
         let a: *const u8 = byte;
-        if unsafe { ioctl(0, libc::TIOCSTI, a) } < 0 {
+        if unsafe { ioctl(0, libc::TIOCSTI.try_into().unwrap(), a) } < 0 {
             panic!("Error encountered when calling ioctl");
         }
     }
