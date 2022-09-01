74ac425b37df2dd68b174fca2e784a0700b756a1
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2022-09-01 17:40:10 -0500

    wip

--- mcfly.ps1
+++ mcfly.ps1
@@ -3,7 +3,7 @@
 # Ensure stdin is a tty
 # Can't
 
-$null = New-Module starship {
+$null = New-Module mcfly {
     if ($env:__MCFLY_LOADED -eq "loaded") {
         return ;
     }
@@ -11,7 +11,7 @@ $null = New-Module starship {
 
     # We need PSReadLine for a number of capabilities
     if ($null -eq (Get-Module -Name PSReadLine)) {
-        Write-Output "Installing PSReadLine for keybindings"
+        Write-Output "Installing PSReadLine as McFly dependency"
         Install-Module PSReadLine
     }
 
@@ -37,15 +37,15 @@ $null = New-Module starship {
     Get-Content $env:HISTFILE | Select-Object -Last 100 | Set-Content $env:MCFLY_HISTORY
 
     <#
-.SYNOPSIS
-Cmdlet to run McFly
+    .SYNOPSIS
+    Cmdlet to run McFly
 
-.PARAMETER CommandToComplete
-The command to complete
+    .PARAMETER CommandToComplete
+    The command to complete
 
-.EXAMPLE
-Invoke-McFly -CommandToComplete "cargo bu"
-#>
+    .EXAMPLE
+    Invoke-McFly -CommandToComplete "cargo bu"
+    #>
     function Invoke-McFly {
         Param([string]$CommandToComplete)
         $lastExitTmp = $LASTEXITCODE
@@ -54,24 +54,27 @@ Invoke-McFly -CommandToComplete "cargo bu"
     }
 
     <#
-.SYNOPSIS
-Add a command to McFly's history.
+    .SYNOPSIS
+    Add a command to McFly's history.
 
-.PARAMETER Command
-The string of the command to add to McFly's history
+    .PARAMETER Command
+    The string of the command to add to McFly's history
 
-.PARAMETER ExitCode
-The exit code of the command to add
+    .PARAMETER ExitCode
+    The exit code of the command to add
 
-.EXAMPLE
-Add-CommandToMcFly -Command "cargo build"
-#>
+    .EXAMPLE
+    Add-CommandToMcFly -Command "cargo build"
+    #>
     function Add-CommandToMcFly {
-        Param([string]$Command)
-        Param([int]$ExitCode)
+        Param (
+            [string] $Command,
+            [int] $ExitCode
+        )
         $ExitCode = $ExitCode ?? 0;
-        Write-Host "Adding Command: $command"
-        Start-Process -FilePath '::MCFLY::'  -ArgumentList add, $command, --exit, $ExitCode -NoNewWindow | Out-Host
+        $Command | Out-File -FilePath $env:MCFLY_HISTORY -Append
+        write-host "Adding Command"
+        Start-Process -FilePath '::MCFLY::' -ArgumentList add, --exit, $ExitCode, --append-to-histfile -NoNewWindow | Write-Host
     }
 
     # We need to make sure we call out AddToHistoryHandler right after each command is called
@@ -79,15 +82,22 @@ Add-CommandToMcFly -Command "cargo build"
 
     Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
-    Set-PSReadLineOption -AddToHistoryHandler {
-        Param([string]$line)
-        # save the exit code to restore in case the program depends on it
+    $historyHandler = {
+        Param([string]$Command)
         $lastExitTmp = $LASTEXITCODE
-        $line = $line.Trim();
+        $Command = $Command.Trim();
         # PSReadLine executes this before the command even runs, so we don't know its exit code - assume 0
-        Add-CommandToMcFly -Command $line -ExitCode 0
-        # Add the command to PSReadLine's real history file
-        Out-File -FilePath $_PSREADLINEHISTORY -Append -InputObject $line
+        Add-CommandToMcFly -Command $Command -ExitCode 0
+        $LASTEXITCODE = $lastExitTmp
+        # Tell PSReadLine to save the command to their in-memory history (and also the dummy file)
+        return $true
+    }
+    Set-PSReadLineOption -AddToHistoryHandler {
+        Param([string]$Command)
+        $lastExitTmp = $LASTEXITCODE
+        $Command = $Command.Trim();
+        # PSReadLine executes this before the command even runs, so we don't know its exit code - assume 0
+        Add-CommandToMcFly -Command $Command -ExitCode 0
         $LASTEXITCODE = $lastExitTmp
         # Tell PSReadLine to save the command to their in-memory history (and also the dummy file)
         return $true
@@ -101,6 +111,6 @@ Add-CommandToMcFly -Command "cargo build"
     }
     Export-ModuleMember -Function @(
         "Invoke-McFly"
-        "Add-LastCommandToMcFly"
+        "Add-CommandToMcFly"
     )
 }
