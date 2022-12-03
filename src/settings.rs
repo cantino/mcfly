f7ad38e4e90230d63a8a39d510a6aa5d96173aba
TD-Sky <three-dim-sky@foxmail.com>
2022-12-03 18:57:09 +0800

    bump clap2 to clap4-derive

--- src/settings.rs
+++ src/settings.rs
@@ -1,7 +1,6 @@
+use crate::cli::{Cli, SubCommand};
 use crate::shell_history;
-use clap::AppSettings;
-use clap::{crate_authors, crate_version, value_t};
-use clap::{App, Arg, SubCommand};
+use clap::Parser;
 use directories_next::{ProjectDirs, UserDirs};
 use std::env;
 use std::path::PathBuf;
@@ -122,136 +121,12 @@ impl Default for Settings {
 
 impl Settings {
     pub fn parse_args() -> Settings {
-        let matches = App::new("McFly")
-            .version(crate_version!())
-            .author(crate_authors!())
-            .about("Fly through your shell history")
-            .setting(AppSettings::SubcommandRequiredElseHelp)
-            .arg(Arg::with_name("debug")
-                .short("d")
-                .long("debug")
-                .help("Debug"))
-            .arg(Arg::with_name("session_id")
-                .long("session_id")
-                .help("Session ID to record or search under (defaults to $MCFLY_SESSION_ID)")
-                .value_name("SESSION_ID")
-                .takes_value(true))
-            .arg(Arg::with_name("mcfly_history")
-                .long("mcfly_history")
-                .help("Shell history file to read from when adding or searching (defaults to $MCFLY_HISTORY)")
-                .value_name("MCFLY_HISTORY")
-                .takes_value(true))
-            .arg(Arg::with_name("history_format")
-                .long("history_format")
-                .help("Shell history file format, 'bash', 'zsh', 'zsh-extended' or 'fish' (defaults to 'bash')")
-                .value_name("FORMAT")
-                .takes_value(true))
-            .subcommand(SubCommand::with_name("add")
-                .about("Add commands to the history")
-                .aliases(&["a"])
-                .arg(Arg::with_name("exit")
-                    .short("e")
-                    .long("exit")
-                    .value_name("EXIT_CODE")
-                    .help("Exit code of command")
-                    .takes_value(true))
-                .arg(Arg::with_name("append_to_histfile")
-                    .long("append-to-histfile")
-                    .help("Also append new history to $HISTFILE/$MCFLY_HISTFILE (e.q., .bash_history)"))
-                .arg(Arg::with_name("when")
-                    .short("w")
-                    .long("when")
-                    .value_name("UNIX_EPOCH")
-                    .help("The time that the command was run (default now)")
-                    .takes_value(true))
-                .arg(Arg::with_name("directory")
-                    .short("d")
-                    .long("dir")
-                    .value_name("PATH")
-                    .help("Directory where command was run (default $PWD)")
-                    .takes_value(true))
-                .arg(Arg::with_name("old_directory")
-                    .short("o")
-                    .long("old-dir")
-                    .value_name("PATH")
-                    .help("The previous directory the user was in before running the command (default $OLDPWD)")
-                    .takes_value(true))
-                .arg(Arg::with_name("command")
-                    .help("The command that was run (default last line of $MCFLY_HISTORY file)")
-                    .value_name("COMMAND")
-                    .multiple(true)
-                    .required(false)
-                    .index(1)))
-            .subcommand(SubCommand::with_name("search")
-                .about("Search the history")
-                .aliases(&["s"])
-                .arg(Arg::with_name("directory")
-                    .short("d")
-                    .long("dir")
-                    .value_name("PATH")
-                    .help("Directory where command was run")
-                    .takes_value(true))
-                .arg(Arg::with_name("results")
-                    .short("r")
-                    .long("results")
-                    .value_name("NUMBER")
-                    .help("Number of results to return")
-                    .takes_value(true))
-                .arg(Arg::with_name("fuzzy")
-                    .short("f")
-                    .long("fuzzy")
-                    .help("Fuzzy-find results. 0 is off; higher numbers weight shorter/earlier matches more. Try 2"))
-                .arg(Arg::with_name("delete_without_confirm")
-                    .long("delete_without_confirm")
-                    .help("Delete entry without confirm"))
-                .arg(Arg::with_name("output_selection")
-                    .short("o")
-                    .long("output-selection")
-                    .value_name("PATH")
-                    .help("Write results to file, including selection mode, new commandline, and any shell-specific requests")
-                    .takes_value(true))
-                .arg(Arg::with_name("command")
-                    .help("The command search term(s)")
-                    .value_name("COMMAND")
-                    .multiple(true)
-                    .required(false)
-                    .index(1)))
-            .subcommand(SubCommand::with_name("move")
-                .about("Record a directory having been moved; moves command records from the old path to the new one")
-                .arg(Arg::with_name("old_dir_path")
-                    .help("The old directory path")
-                    .value_name("OLD_DIR_PATH")
-                    .multiple(false)
-                    .required(true)
-                    .index(1))
-                .arg(Arg::with_name("new_dir_path")
-                    .help("The new directory path")
-                    .value_name("NEW_DIR_PATH")
-                    .multiple(false)
-                    .required(true)
-                    .index(2)))
-            .subcommand(SubCommand::with_name("train")
-                .about("Train the suggestion engine (developer tool)")
-                .arg(Arg::with_name("refresh_cache")
-                    .short("r")
-                    .long("refresh_cache")
-                    .help("Directory where command was run")
-                    .required(false)))
-            .subcommand(SubCommand::with_name("init")
-                .about("Prints the shell code used to execute mcfly")
-                .arg(Arg::with_name("shell")
-                    .help("Shell to init — one of bash, zsh, or fish")
-                    .possible_values(&["bash", "zsh", "fish"])
-                    .required(true))
-            )
-            .get_matches();
+        let cli = Cli::parse();
 
         let mut settings = Settings::default();
-        if matches.is_present("init") {
-            settings.skip_environment_check = true;
-        }
+        settings.skip_environment_check = cli.is_init();
 
-        settings.debug = matches.is_present("debug") || env::var("MCFLY_DEBUG").is_ok();
+        settings.debug = cli.debug || env::var("MCFLY_DEBUG").is_ok();
         settings.limit = env::var("MCFLY_HISTORY_LIMIT")
             .ok()
             .and_then(|o| o.parse::<i64>().ok());
@@ -274,93 +149,83 @@ impl Settings {
             _ => ResultSort::Rank,
         };
 
-        settings.session_id = matches
-            .value_of("session_id")
-            .map(|s| s.to_string())
-            .unwrap_or_else( ||
-                env::var("MCFLY_SESSION_ID")
-                    .unwrap_or_else(|err| {
-                        if !settings.skip_environment_check
-                        {
-                            panic!(
+        settings.session_id = cli.session_id.unwrap_or_else(||
+            env::var("MCFLY_SESSION_ID")
+                .unwrap_or_else(|err| {
+                    if !settings.skip_environment_check {
+                        panic!(
                             "McFly error: Please ensure that MCFLY_SESSION_ID contains a random session ID ({})",
-                            err)
-                        }
-                        else {
-                            std::string::String::new()
-                        }
-                    }));
-        settings.mcfly_history = PathBuf::from(
-            matches
-                .value_of("mcfly_history")
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| {
-                    env::var("MCFLY_HISTORY").unwrap_or_else(|err| {
-                        if !settings.skip_environment_check {
-                            panic!(
-                                "McFly error: Please ensure that MCFLY_HISTORY is set ({})",
-                                err
-                            )
-                        } else {
-                            std::string::String::new()
-                        }
-                    })
-                }),
+                            err
+                        )
+                    } else {
+                        String::new()
+                    }
+                }
+            )
         );
-        settings.history_format = match matches.value_of("history_format") {
-            None => HistoryFormat::Bash,
-            Some("bash") => HistoryFormat::Bash,
-            Some("zsh") => HistoryFormat::Zsh {
-                extended_history: false,
-            },
-            Some("zsh-extended") => HistoryFormat::Zsh {
-                extended_history: true,
-            },
-            Some("fish") => HistoryFormat::Fish,
-            Some(format) => panic!("McFly error: unknown history format '{}'", format),
-        };
 
-        match matches.subcommand() {
-            ("add", Some(add_matches)) => {
+        settings.mcfly_history = cli.mcfly_history.unwrap_or_else(|| {
+            {
+                env::var("MCFLY_HISTORY").unwrap_or_else(|err| {
+                    if !settings.skip_environment_check {
+                        panic!(
+                            "McFly error: Please ensure that MCFLY_HISTORY is set ({})",
+                            err
+                        )
+                    } else {
+                        String::new()
+                    }
+                })
+            }
+            .into()
+        });
+
+        {
+            use crate::cli::HistoryFormat::*;
+            settings.history_format = match cli.history_format {
+                Bash => HistoryFormat::Bash,
+                Zsh => HistoryFormat::Zsh {
+                    extended_history: false,
+                },
+                ZshExtended => HistoryFormat::Zsh {
+                    extended_history: true,
+                },
+                Fish => HistoryFormat::Fish,
+            };
+        }
+
+        match cli.command {
+            SubCommand::Add {
+                command,
+                exit,
+                append_to_histfile,
+                when,
+                directory,
+                old_directory,
+            } => {
                 settings.mode = Mode::Add;
 
-                settings.when_run = Some(
-                    value_t!(add_matches, "when", i64).unwrap_or(
+                settings.when_run = when.or_else(|| {
+                    Some(
                         SystemTime::now()
                             .duration_since(UNIX_EPOCH)
                             .unwrap_or_else(|err| {
                                 panic!("McFly error: Time went backwards ({})", err)
                             })
                             .as_secs() as i64,
-                    ),
-                );
+                    )
+                });
 
-                settings.append_to_histfile = add_matches.is_present("append_to_histfile");
+                settings.append_to_histfile = append_to_histfile;
 
-                if add_matches.value_of("exit").is_some() {
-                    settings.exit_code =
-                        Some(value_t!(add_matches, "exit", i32).unwrap_or_else(|e| e.exit()));
-                }
+                settings.exit_code = exit;
 
-                if let Some(dir) = add_matches.value_of("directory") {
-                    settings.dir = dir.to_string();
-                } else {
-                    settings.dir = env::var("PWD").unwrap_or_else(|err| {
-                        panic!(
-                            "McFly error: Unable to determine current directory ({})",
-                            err
-                        )
-                    });
-                }
+                settings.dir = directory.unwrap_or_else(pwd);
 
-                if let Some(old_dir) = add_matches.value_of("old_directory") {
-                    settings.old_dir = Some(old_dir.to_string());
-                } else {
-                    settings.old_dir = env::var("OLDPWD").ok();
-                }
+                settings.old_dir = old_directory.or_else(|| env::var("OLDPWD").ok());
 
-                if let Some(commands) = add_matches.values_of("command") {
-                    settings.command = commands.collect::<Vec<_>>().join(" ");
+                if !command.is_empty() {
+                    settings.command = command.join(" ");
                 } else {
                     settings.command = shell_history::last_history_line(
                         &settings.mcfly_history,
@@ -374,29 +239,29 @@ impl Settings {
                     || settings.command.starts_with("pushd ")
                     || settings.command.starts_with("j ")
                 {
-                    settings.dir = settings.old_dir.to_owned().unwrap_or(settings.dir);
+                    settings.dir = settings.old_dir.clone().unwrap_or(settings.dir);
                 }
             }
 
-            ("search", Some(search_matches)) => {
+            SubCommand::Search {
+                command,
+                directory,
+                results,
+                fuzzy,
+                delete_without_confirm,
+                output_selection,
+            } => {
                 settings.mode = Mode::Search;
-                if let Some(dir) = search_matches.value_of("directory") {
-                    settings.dir = dir.to_string();
-                } else {
-                    settings.dir = env::var("PWD").unwrap_or_else(|err| {
-                        panic!(
-                            "McFly error: Unable to determine current directory ({})",
-                            err
-                        )
-                    });
-                }
+
+                settings.dir = directory.unwrap_or_else(pwd);
 
                 if let Ok(results) = env::var("MCFLY_RESULTS") {
                     if let Ok(results) = u16::from_str(&results) {
                         settings.results = results;
                     }
                 }
-                if let Ok(results) = value_t!(search_matches.value_of("results"), u16) {
+
+                if let Some(results) = results {
                     settings.results = results;
                 }
 
@@ -407,21 +272,18 @@ impl Settings {
                         settings.fuzzy = 2;
                     }
                 }
-                if let Ok(fuzzy) = value_t!(search_matches.value_of("fuzzy"), i16) {
+
+                if let Some(fuzzy) = fuzzy {
                     settings.fuzzy = fuzzy;
-                } else if search_matches.is_present("fuzzy") {
-                    settings.fuzzy = 2;
                 }
 
-                settings.delete_without_confirm = search_matches
-                    .is_present("delete_without_confirm")
-                    || env::var("MCFLY_DELETE_WITHOUT_CONFIRM").is_ok();
-                settings.output_selection = search_matches
-                    .value_of("output_selection")
-                    .map(|s| s.to_owned());
+                settings.delete_without_confirm =
+                    delete_without_confirm || env::var("MCFLY_DELETE_WITHOUT_CONFIRM").is_ok();
 
-                if let Some(values) = search_matches.values_of("command") {
-                    settings.command = values.collect::<Vec<_>>().join(" ");
+                settings.output_selection = output_selection;
+
+                if !command.is_empty() {
+                    settings.command = command.join(" ");
                 } else {
                     settings.command = shell_history::last_history_line(
                         &settings.mcfly_history,
@@ -439,54 +301,33 @@ impl Settings {
                 }
             }
 
-            ("train", Some(train_matches)) => {
+            SubCommand::Train { refresh_cache } => {
                 settings.mode = Mode::Train;
-                settings.refresh_training_cache = train_matches.is_present("refresh_cache");
+                settings.refresh_training_cache = refresh_cache;
             }
 
-            ("move", Some(move_matches)) => {
+            SubCommand::Move {
+                old_dir_path,
+                new_dir_path,
+            } => {
                 settings.mode = Mode::Move;
-                settings.old_dir = Some(String::from(
-                    move_matches
-                        .value_of("old_dir_path")
-                        .unwrap_or_else(|| panic!("McFly error: Expected value for old_dir_path")),
-                ));
-                settings.dir = String::from(
-                    move_matches
-                        .value_of("new_dir_path")
-                        .unwrap_or_else(|| panic!("McFly error: Expected value for new_dir_path")),
-                );
+                settings.old_dir = Some(old_dir_path);
+                settings.dir = new_dir_path;
             }
 
-            ("init", Some(init_matches)) => {
-                settings.mode = Mode::Init;
-                match init_matches.value_of("shell").unwrap() {
-                    "bash" => {
-                        settings.init_mode = InitMode::Bash;
-                    }
-                    "zsh" => {
-                        settings.init_mode = InitMode::Zsh;
-                    }
-                    "fish" => {
-                        settings.init_mode = InitMode::Fish;
-                    }
-                    _ => unreachable!(),
-                }
+            SubCommand::Init { shell } => {
+                use crate::cli::InitMode::*;
+                settings.init_mode = match shell {
+                    Bash => InitMode::Bash,
+                    Zsh => InitMode::Zsh,
+                    Fish => InitMode::Fish,
+                };
             }
-
-            ("", None) => println!("No subcommand was used"), // If no subcommand was used it'll match the tuple ("", None)
-            _ => unreachable!(), // If all subcommands are defined above, anything else is unreachable!()
         }
 
-        settings.lightmode = match env::var_os("MCFLY_LIGHT") {
-            Some(_val) => true,
-            None => false,
-        };
+        settings.lightmode = env::var_os("MCFLY_LIGHT").is_some();
 
-        settings.disable_menu = match env::var_os("MCFLY_DISABLE_MENU") {
-            Some(_val) => true,
-            None => false,
-        };
+        settings.disable_menu = env::var_os("MCFLY_DISABLE_MENU").is_some();
 
         settings.key_scheme = match env::var("MCFLY_KEY_SCHEME").as_ref().map(String::as_ref) {
             Ok("vim") => KeyScheme::Vim,
@@ -524,10 +365,15 @@ impl Settings {
             .home_dir()
             .join(PathBuf::from(".mcfly"));
 
-        if user_dirs_file.exists() {
-            Some(user_dirs_file)
-        } else {
-            None
-        }
+        user_dirs_file.exists().then_some(user_dirs_file)
     }
+}
+
+fn pwd() -> String {
+    env::var("PWD").unwrap_or_else(|err| {
+        panic!(
+            "McFly error: Unable to determine current directory ({})",
+            err
+        )
+    })
 }
