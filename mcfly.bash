ceff195198a09939c3ea0766a054c4fc0cffcc35
Koichi Murase <myoga.murase@gmail.com>
2024-07-08 15:11:32 +0900

    Use [[ -o vi ]] to test the vi editing mode in Bash

--- mcfly.bash
+++ mcfly.bash
@@ -128,7 +128,7 @@ function mcfly_initialize {
     #   2. Type "mcfly search" and then run the command. McFly will pull the last line from the $MCFLY_HISTORY file,
     #      which should be the commented-out search from step #1. It will then remove that line from the history file and
     #      render the search UI pre-filled with it.
-    if set -o | grep "vi " | grep -q on; then
+    if [[ -o vi ]]; then
       bind '"\C-r": "\e0i#mcfly: \e\C-m mcfly search\C-m"'
     else
       bind '"\C-r": "\C-amcfly: \e# mcfly search\C-m"'
